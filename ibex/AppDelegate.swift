acd20cdc26bb29add8c138043194ad7345238255
Charlotte <Charlotte@charlottes-macbook-pro.local>
2015-11-01 20:28:04 +0100

    Initial Commit

---
+++ ibex/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ibex
+//
+//  Created by Charlotte on 01/11/15.
+//  Copyright © 2015 Charlotte. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
