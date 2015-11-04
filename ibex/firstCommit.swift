a8c13af207e7afdec557df9c02892daeabe17004
Charlotte <Charlotte@charlottes-macbook-pro.local>
2015-11-04 21:55:46 +0100

    We create in this commit our first commit

---
+++ ibex/firstCommit.swift
@@ -0,0 +1,11 @@
+//
+//  firstCommit.swift
+//  ibex
+//
+//  Created by Charlotte on 04/11/15.
+//  Copyright © 2015 Charlotte. All rights reserved.
+//
+
+import Foundation
+
+// Charlotte is learning GIT
